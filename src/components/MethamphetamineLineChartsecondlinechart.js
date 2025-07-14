7a72d738e4fc9ce76d03214676c23cb2aaa8465b
Sravan Velampalli <tnj7@cdc.gov>
2025-07-14 15:15:47 -0400

    Using Json data

--- src/components/MethamphetamineLineChartsecondlinechart.js
+++ src/components/MethamphetamineLineChartsecondlinechart.js
@@ -8,81 +8,6 @@ import { UtilityFunctions } from '../utility';
 import { allQuarters, allPeriods6M } from '../constants/Constants';
 
 
-// Quarterly data for National
-const methNationalSecondData = [
-  // Fentanyl
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q4 2022', percentage: 48.7, ciLower: 47.7, ciUpper: 49.7 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q1 2023', percentage: 45.3, ciLower: 44.4, ciUpper: 46.3 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q2 2023', percentage: 46.7, ciLower: 45.8, ciUpper: 47.7 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q3 2023', percentage: 48.1, ciLower: 47.2, ciUpper: 49.1 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q4 2023', percentage: 49, ciLower: 48, ciUpper: 49.9 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q1 2024', percentage: 47.7, ciLower: 46.8, ciUpper: 48.6 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q2 2024', percentage: 48.1, ciLower: 47.2, ciUpper: 49 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q3 2024', percentage: 48.1, ciLower: 47.2, ciUpper: 49 },
-  { region: 'National', drug: 'Fentanyl', quarter: 'Q4 2024', percentage: 47, ciLower: 46.1, ciUpper: 48 },
-
-  // Heroin
-  { region: 'National', drug: 'Heroin', quarter: 'Q4 2022', percentage: 13.9, ciLower: 13.2, ciUpper: 14.6 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q1 2023', percentage: 11.8, ciLower: 11.1, ciUpper: 12.4 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q2 2023', percentage: 12.1, ciLower: 11.5, ciUpper: 12.8 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q3 2023', percentage: 11, ciLower: 10.4, ciUpper: 11.6 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q4 2023', percentage: 10.5, ciLower: 9.9, ciUpper: 11.1 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q1 2024', percentage: 10.2, ciLower: 9.6, ciUpper: 10.8 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q2 2024', percentage: 10.3, ciLower: 9.8, ciUpper: 10.9 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q3 2024', percentage: 13.9, ciLower: 13.3, ciUpper: 14.5 },
-  { region: 'National', drug: 'Heroin', quarter: 'Q4 2024', percentage: 15.3, ciLower: 14.7, ciUpper: 15.9 },
-
-  // Opioids
-  { region: 'National', drug: 'Opioids', quarter: 'Q4 2022', percentage: 51.4, ciLower: 50.4, ciUpper: 52.4 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q1 2023', percentage: 47.3, ciLower: 46.3, ciUpper: 48.3 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q2 2023', percentage: 48.6, ciLower: 47.7, ciUpper: 49.6 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q3 2023', percentage: 49.7, ciLower: 48.7, ciUpper: 50.7 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q4 2023', percentage: 50.6, ciLower: 49.7, ciUpper: 51.6 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q1 2024', percentage: 49.1, ciLower: 48.2, ciUpper: 50 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q2 2024', percentage: 50.5, ciLower: 49.6, ciUpper: 51.4 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q3 2024', percentage: 49.6, ciLower: 48.7, ciUpper: 50.5 },
-  { region: 'National', drug: 'Opioids', quarter: 'Q4 2024', percentage: 48.7, ciLower: 47.9, ciUpper: 49.6 },
-
-  // Cocaine
-  { region: 'National', drug: 'Cocaine', quarter: 'Q4 2022', percentage: 12, ciLower: 11.4, ciUpper: 12.7 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q1 2023', percentage: 12, ciLower: 11.4, ciUpper: 12.7 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q2 2023', percentage: 13.4, ciLower: 12.8, ciUpper: 14.1 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q3 2023', percentage: 13.3, ciLower: 12.8, ciUpper: 13.7 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q4 2023', percentage: 14.7, ciLower: 14, ciUpper: 15.3 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q1 2024', percentage: 16.2, ciLower: 15.6, ciUpper: 16.9 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q2 2024', percentage: 17.8, ciLower: 17.1, ciUpper: 18.4 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q3 2024', percentage: 17.8, ciLower: 17.1, ciUpper: 18.4 },
-  { region: 'National', drug: 'Cocaine', quarter: 'Q4 2024', percentage: 16.8, ciLower: 16.2, ciUpper: 17.5 },
-];
-
-// 6 Months data for National
-const methNationalSecondData6Months = [
-  // Fentanyl
-  { region: 'National', drug: 'Fentanyl', period: 'Jul-Dec 2022', percentage: 48.7, ciLower: 47.7, ciUpper: 49.7 },
-  { region: 'National', drug: 'Fentanyl', period: 'Jan-Jun 2023', percentage: 46, ciLower: 45.4, ciUpper: 46.7 },
-  { region: 'National', drug: 'Fentanyl', period: 'Jul-Dec 2023', percentage: 48.5, ciLower: 47.9, ciUpper: 49.2 },
-  { region: 'National', drug: 'Fentanyl', period: 'Jan-Jun 2024', percentage: 48.4, ciLower: 47.7, ciUpper: 49 },
-  { region: 'National', drug: 'Fentanyl', period: 'Jul-Dec 2024', percentage: 47.5, ciLower: 46.9, ciUpper: 48.1 },
-  // Heroin
-  { region: 'National', drug: 'Heroin', period: 'Jul-Dec 2022', percentage: 13.9, ciLower: 13.2, ciUpper: 14.6 },
-  { region: 'National', drug: 'Heroin', period: 'Jan-Jun 2023', percentage: 12, ciLower: 11.5, ciUpper: 12.4 },
-  { region: 'National', drug: 'Heroin', period: 'Jul-Dec 2023', percentage: 10.8, ciLower: 10.4, ciUpper: 11.2 },
-  { region: 'National', drug: 'Heroin', period: 'Jan-Jun 2024', percentage: 10.3, ciLower: 9.9, ciUpper: 10.7 },
-  { region: 'National', drug: 'Heroin', period: 'Jul-Dec 2024', percentage: 14.6, ciLower: 14.2, ciUpper: 15 },
-  // Opioids
-  { region: 'National', drug: 'Opioids', period: 'Jul-Dec 2022', percentage: 51.4, ciLower: 50.4, ciUpper: 52.4 },
-  { region: 'National', drug: 'Opioids', period: 'Jan-Jun 2023', percentage: 48, ciLower: 47.3, ciUpper: 48.7 },
-  { region: 'National', drug: 'Opioids', period: 'Jul-Dec 2023', percentage: 50.2, ciLower: 49.5, ciUpper: 50.8 },
-  { region: 'National', drug: 'Opioids', period: 'Jan-Jun 2024', percentage: 49.8, ciLower: 49.2, ciUpper: 50.5 },
-  { region: 'National', drug: 'Opioids', period: 'Jul-Dec 2024', percentage: 49.1, ciLower: 48.5, ciUpper: 49.7 },
-  // Cocaine
-  { region: 'National', drug: 'Cocaine', period: 'Jul-Dec 2022', percentage: 11.6, ciLower: 11, ciUpper: 12.3 },
-  { region: 'National', drug: 'Cocaine', period: 'Jan-Jun 2023', percentage: 12.6, ciLower: 12.1, ciUpper: 13 },
-  { region: 'National', drug: 'Cocaine', period: 'Jul-Dec 2023', percentage: 13.5, ciLower: 13.1, ciUpper: 14 },
-  { region: 'National', drug: 'Cocaine', period: 'Jan-Jun 2024', percentage: 15.5, ciLower: 15, ciUpper: 16 },
-  { region: 'National', drug: 'Cocaine', period: 'Jul-Dec 2024', percentage: 17.3, ciLower: 16.8, ciUpper: 17.8 },
-];
-
 const lineColors = {
   'Fentanyl': '#27ae60',
   'Heroin': '#8e44ad',
@@ -105,6 +30,9 @@ function alignDataToQuarters(data, quarters, labelField = 'quarter') {
 const MethamphetamineLineChartsecondLineChart = ({ width, height = 350, period }) => {
   const [showLabels, setShowLabels] = useState(false);
   const [showPercentChange, setShowPercentChange] = useState(false);
+  const [methNationalSecondData, setMethNationalSecondData] = useState([]);
+
+
   const [selectedLines, setSelectedLines] = useState(Object.keys(lineColors));
   const allLineKeys = Object.keys(lineColors);
 
@@ -112,10 +40,46 @@ const MethamphetamineLineChartsecondLineChart = ({ width, height = 350, period }
   const adjustedWidth = width - margin.left - margin.right;
   const adjustedHeight = height - margin.top - margin.bottom;
 
+  useEffect(() => {
+    ReactTooltip.rebuild();
+  }, [selectedLines, showPercentChange, showLabels]);
+
+  useEffect(() => {
+        fetch(process.env.PUBLIC_URL + '/data/Millenial-Format.normalized.json')
+          .then(res => res.json())
+          .then(data => {
+  
+            var methNationalSecondData = [];
+
+            const nfData = UtilityFunctions.getGroupedData(data, 'National', 'Fentanyl', 'Positivity', period, ['Fentanyl']);
+            const nhData = UtilityFunctions.getGroupedData(data, 'National', 'Heroin', 'Positivity', period, ['Heroin']);
+            const nmData = UtilityFunctions.getGroupedData(data, 'National', 'Methamphetamine', 'CoPositive', period, ['Opioids']);
+            const ncData = UtilityFunctions.getGroupedData(data, 'National', 'Cocaine', 'Positivity', period, ['Cocaine']);
+  
+           if (nfData.length > 0) {
+                for(var i=0; i<nfData[0].data.length; i++)
+                  methNationalSecondData.push(nfData[0].data[i])
+            }
+            if (nhData.length > 0) {
+                for(var i=0; i<nhData[0].data.length; i++)
+                  methNationalSecondData.push(nhData[0].data[i])
+            }
+            if (nmData.length > 0) {
+                for(var i=0; i<nmData[0].data.length; i++)
+                  methNationalSecondData.push(nmData[0].data[i])
+            }
+            if (ncData.length > 0) {
+                for(var i=0; i<ncData[0].data.length; i++)
+                  methNationalSecondData.push(ncData[0].data[i])
+            }
+
+            setMethNationalSecondData(methNationalSecondData);
+          });
+    
+      }, []);
+  
   const is6M = period === 'HalfYearly';
-  const regionData = is6M
-    ? methNationalSecondData6Months.filter(d => d.region === 'National')
-    : methNationalSecondData.filter(d => d.region === 'National');
+  const regionData = methNationalSecondData;
   const xDomain = is6M ? allPeriods6M : allQuarters;
   const xLabelField = is6M ? 'period' : 'quarter';
   const datasets = alignDataToQuarters(regionData, xDomain, xLabelField);
@@ -154,27 +118,6 @@ const MethamphetamineLineChartsecondLineChart = ({ width, height = 350, period }
       };
     }
   }
-
-  useEffect(() => {
-    ReactTooltip.rebuild();
-  }, [selectedLines, showPercentChange, showLabels]);
-
-  useEffect(() => {
-        fetch(process.env.PUBLIC_URL + '/data/Millenial-Format.normalized.json')
-          .then(res => res.json())
-          .then(data => {
-  
-            const nhData = UtilityFunctions.getGroupedData(data, 'National', 'Fentanyl', 'Positivity', period, ['Heroin']);
-            const ncData = UtilityFunctions.getGroupedData(data, 'National', 'Heroin', 'Positivity', period, ['Cocaine']);
-            const nmData = UtilityFunctions.getGroupedData(data, 'National', 'Opioids', 'Positivity', period, ['Methamphetamine']);
-            const nfData = UtilityFunctions.getGroupedData(data, 'National', 'Cocaine', 'CoPositive', period, ['Fentanyl and Stimulants']);
-  
-            //const fentanylNatData = [{name: 'Fentanyl', values: nData[0].data}, {name: 'Fentanyl with Stimulants', values: nData[1].data}, {name: 'Fentanyl without Stimulants', values: nData[2].data}]
-            //setFentanylNationalData(fentanylNatData);
-          });
-    
-      }, []);
-  
 
   const percentChgTooltip = `
     <div style="
